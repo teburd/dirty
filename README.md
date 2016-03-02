822e3467fe3b9b664d1a6710a58f21b2c95f9dfd
Tom Burdick <thomas.burdick@gmail.com>
2016-03-02 15:24:09 -0600

    Autopublish docs
    * Add travis badge

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dirty
 Dirty Container for Rust
+
+[![Build Status](https://travis-ci.org/bfrog/dirty.svg?branch=master)](https://travis-ci.org/bfrog/dirty)
---
+++ publish_docs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+    echo "This commit was part of a pull request not a merge to master. Not publishing docs"
+    exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+    echo "This commit was made against the $TRAVIS_BRANCH and not the master! Not publishing docs"
+    exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd target/docs
+
+echo '<meta http-equiv="refresh" content="0; url=dirty/index.html">' > index.html
+
+git init
+git config user.name "Tom Burdick"
+git config user.email "thomas.burdick@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/bfrog/dirty.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
